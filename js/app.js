3349ee90f3271d29c8d0853881cb711d43c061c6
Lucaponz91 <lucapo1991@msn.com>
2022-08-23 15:18:46 +0200

    funge!

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#root',
+    data:{
+        testo: 'Pagina di prova utilizzando Vue',
+        immagine: 'https://picsum.photos/200',
+        alt: 'immagine a caso'
+    },
+    
+})
